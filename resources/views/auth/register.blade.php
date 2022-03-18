b23916df7f0eb66c6f905e416cf6b3815d63eafc
Hazrat bilal <hazratbilalhabibi123@gmail.com>
2022-03-18 07:53:57 +0000

    login page has corrected

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,17 +4,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header text">
+                        <h2> Créer un nouveau compte </h2>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Prénom') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Entrez votre nom *">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -25,10 +27,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adresse mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Entrez votre mail *">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -39,10 +41,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Entrez votre mot de passe *">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -53,17 +55,17 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmer le mot de passe') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmer votre mot de passe *">
                             </div>
                         </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    Créer un nouveau compte
                                 </button>
                             </div>
                         </div>
--- resources/views/utilisateurs/creer_utilisateurs.blade.php
+++ resources/views/utilisateurs/creer_utilisateurs.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"> 
-                    <h3> Enregistrement </h3>
+                    <h3> Créer un nouveau compte utilisateur </h3>
                 </div>
 
                 <div class="card-body">
@@ -17,7 +17,7 @@
                         <div class="form-group row mb-2">
                             <label for="inputName" class="col-sm-3 col-form-label">Prénom</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="name" id="inputName" placeholder="Nom" value="{{old('name')}}">
+                                <input type="text" class="form-control" name="name" id="inputName" placeholder="Entrez le nom *" value="{{old('name')}}">
                             </div>
                         </div>
 
@@ -26,21 +26,21 @@
                         <div class="form-group row mb-2">
                             <label for="inputEmail" class="col-sm-3 col-form-label">Mail</label>
                             <div class="col-sm-9">
-                                <input type="email" class="form-control" name="email" id="inputEmail" placeholder="E-mail" value="{{old('email')}}">
+                                <input type="email" class="form-control" name="email" id="inputEmail" placeholder="Entrez l'adresse mail *" value="{{old('email')}}">
                             </div>
                         </div>
 
                         <div class="form-group row mb-2">
                             <label for="inputPassword" class="col-sm-3 col-form-label">Mot de passe</label>
                             <div class="col-sm-9">
-                                <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password">
+                                <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Choisissez un mot de passe *">
                             </div>
                         </div>
 
                         <div class="form-group row mb-2">
-                            <label for="inputConfirmPassword" class="col-sm-3 col-form-label">Confirme mot de passe</label>
+                            <label for="inputConfirmPassword" class="col-sm-3 col-form-label">Confirme le mot de passe</label>
                             <div class="col-sm-9">
-                                <input type="password" class="form-control" name="password_confirmation" id="inputConfirmPassword" placeholder="Confirm Password">
+                                <input type="password" class="form-control" name="password_confirmation" id="inputConfirmPassword" placeholder="Confirmer le mot de passe *">
                             </div>
                         </div>
 
@@ -48,7 +48,7 @@
 
                         <div class="form-group row mt-4">
                             <div class="col-sm-10">
-                            <button type="submit" class="btn btn-success"> <i class="fa fa-user-plus"></i> Ajouter </button>
+                            <button type="submit" class="btn btn-success"> <i class="fa fa-user-plus"></i> Créer le compte </button>
                             </div>
                         </div>
 
